6e4c1805d802030bb2186d07f1fca74ef10e7a44
Kue Xiong <kuexiong14@gmail.com>
2023-04-01 23:10:35 -0500

    test(util): Update sql script
    Updated sql script due to change in database structure.

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,7 +1,5 @@
 delete from user;
-delete from plant;
-INSERT INTO `plant` VALUES (1,'Peace Lily'),
-                           (2,'Mini Monstera');
-INSERT INTO user VALUES (1,'Kue','Xiong','kxiong',1),
-                        (2, 'Spongebob', 'Square Pants', 'ssquarepants',2);
+
+INSERT INTO user VALUES (1,'Kue','Xiong','kxiong'),
+                        (2, 'Spongebob', 'Square Pants', 'ssquarepants');
 

b569bc2f5ca9b9e33288a869d00003ce3f5af7f7
Kue Xiong <kuexiong14@gmail.com>
2023-04-01 22:23:43 -0500

    test(/): Add database files

---
+++ src/test/java/com.kueerickpatrick.plantinfo.test.util/Database.java
@@ -0,0 +1,123 @@
+package com.kueerickpatrick.plantinfo.test.util;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * Provides access the database
+ * Created on 8/31/16.
+ *
+ * @author pwaite
+ */
+
+public class Database {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    // create an object of the class Database
+    private static Database instance = new Database();
+
+    private Properties properties;
+
+    private Connection connection;
+
+    // private constructor prevents instantiating this class anywhere else
+    private Database() {
+        loadProperties();
+
+    }
+
+    private void loadProperties() {
+        properties = new Properties();
+        try {
+            properties.load (this.getClass().getResourceAsStream("/database.properties"));
+        } catch (IOException ioe) {
+            logger.error("IO Exception: ", ioe);
+        } catch (Exception e) {
+            logger.error("Exception error: ", e);
+        }
+
+    }
+
+    // get the only Database object available
+    public static Database getInstance() {
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void connect() throws Exception {
+        if (connection != null)
+            return;
+
+        try {
+            Class.forName(properties.getProperty("driver"));
+        } catch (ClassNotFoundException e) {
+            throw new Exception("Database.connect()... Error: MySQL Driver not found");
+        }
+
+        String url = properties.getProperty("url");
+        connection = DriverManager.getConnection(url, properties.getProperty("username"),  properties.getProperty("password"));
+    }
+
+    public void disconnect() {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                logger.error("SQL Exception: ", e);
+            }
+        }
+
+        connection = null;
+    }
+
+    /**
+     * Run the sql.
+     *
+     * @param sqlFile the sql file to be read and executed line by line
+     */
+    public void runSQL(String sqlFile) {
+
+        Statement stmt = null;
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classloader.getResourceAsStream(sqlFile);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connect();
+            stmt = connection.createStatement();
+            String sql = "";
+            while (br.ready())
+            {
+                char inputValue = (char)br.read();
+                if(inputValue == ';')
+                {
+                    stmt.executeUpdate(sql);
+                    sql = "";
+                }
+                else
+                    sql += inputValue;
+            }
+        } catch (SQLException se) {
+            logger.error(se);
+        } catch (Exception e) {
+            logger.error(e);
+        } finally {
+            disconnect();
+        }
+
+    }
+}

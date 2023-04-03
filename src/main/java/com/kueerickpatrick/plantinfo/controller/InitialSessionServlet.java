2fcea3c98fa2cc6f24aae29dda447a5a73608e5d
pgfrank1 <patrickfrank1995@gmail.com>
2023-04-03 15:02:21 -0500

    docs(controller): Added comments
    Author: pgfrank1

--- src/main/java/com/kueerickpatrick/plantinfo/controller/InitialSessionServlet.java
+++ src/main/java/com/kueerickpatrick/plantinfo/controller/InitialSessionServlet.java
@@ -17,12 +17,36 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 
+/**
+ * This servlet gets used when any user connects to the application
+ * It gathers data needed to display on the index page.
+ *
+ * @author PGFrank1
+ * @version 1.0
+ */
 @WebServlet (
         name = "InitialSessionServlet",
         urlPatterns = {"/index.jsp"}
 )
 public class InitialSessionServlet extends HttpServlet {
     private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     *
+     * Retrieves the plant detail list, maps the JSON response from Perenual, and places the data within the
+     * plantDetailMap request attribute
+     *
+     * @param req   an {@link HttpServletRequest} object that
+     *                  contains the request the client has made
+     *                  of the servlet
+     *
+     * @param resp  an {@link HttpServletResponse} object that
+     *                  contains the response the servlet sends
+     *                  to the client
+     *
+     * @throws ServletException
+     * @throws IOException
+     */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Client client = ClientBuilder.newClient();

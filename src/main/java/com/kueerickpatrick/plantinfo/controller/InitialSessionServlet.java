b76b152c100964ca5330b55833765419a968ac0e
pgfrank1 <patrickfrank1995@gmail.com>
2023-04-03 12:33:54 -0500

    feat(controller): Created 2 servlets for Sessions and Application launch
    StartupServlet places the needed properties within servlet context attributes. InitialSessionServlet gets the plant data and places it into a request attribute so new info can be populated.
    
    Author: pgfrank1

---
+++ src/main/java/com/kueerickpatrick/plantinfo/controller/InitialSessionServlet.java
@@ -0,0 +1,44 @@
+package com.kueerickpatrick.plantinfo.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kueerickpatrick.plantinfo.entity.plantObjects.PlantDetailList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+
+public class InitialSessionServlet extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Client client = ClientBuilder.newClient();
+        String plantInfoApiUrl = (String) getServletContext().getAttribute("plantListMainPageUrl") + 1
+                + "&" + getServletContext().getAttribute("apiKey");
+        logger.debug("Attempting to retrieve plant details pages");
+
+        WebTarget targetPlantDetails = client.target(plantInfoApiUrl);
+        String apiResponse = targetPlantDetails.request(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        PlantDetailList plantDetailList = mapper.readValue(apiResponse, PlantDetailList.class);
+
+        req.setAttribute("plantDetailMap", plantDetailList);
+
+        String url = "/WEB-INF/index.jsp";
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
---
+++ src/main/java/com/kueerickpatrick/plantinfo/controller/StartupServlet.java
@@ -0,0 +1,59 @@
+package com.kueerickpatrick.plantinfo.controller;
+
+import com.kueerickpatrick.plantinfo.util.PropertiesLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * This class places all the properties that are needed within the application scope. It reduces the need to have a
+ * properties loader within each class.
+ * @author PGFrank1
+ * @version 1.0
+ */
+@WebListener
+public class StartupServlet extends HttpServlet implements PropertiesLoader, ServletContextListener {
+    /**
+     * Logs the errors that occur within the StartupServlet class.
+     */
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    /**
+     * Holds the properties that needs to be loaded for the application.
+     */
+    Properties properties;
+
+    /**
+     * This method obtains the initial servlet context and assigns the needed properties for the
+     * application to servlet attributes.
+     *
+     * @param sce the ServletContextEvent containing the ServletContext
+     * that is being initialized
+     */
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            properties = loadProperties("/database.properties");
+
+            properties = loadProperties("/perenualAPI.properties");
+            sce.getServletContext().setAttribute("apiKey", properties.getProperty("api.key"));
+            sce.getServletContext().setAttribute("plantListMainPageUrl", properties.getProperty("plant.list.main.page"));
+            sce.getServletContext().setAttribute("plantDetailsUrl", properties.getProperty("plant.details.link"));
+
+        } catch (IOException e) {
+            logger.error("There was an error attempting to open a properties file: ", e);
+        } catch (Exception e) {
+            logger.error("There was a general error that occurred when attempting to use the properties file: ", e);
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}

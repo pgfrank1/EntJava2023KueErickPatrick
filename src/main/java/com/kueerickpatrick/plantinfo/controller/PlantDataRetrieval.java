88adaaf0db2ba9a2b5f807e972bcc10591500ae3
pgfrank1 <patrickfrank1995@gmail.com>
2023-04-06 15:28:57 -0500

    refactor(controller): PlantDataRetrieval now successfully utilizes properties and is able to store Plant data items within a list
    Author: pgfrank1

--- src/main/java/com/kueerickpatrick/plantinfo/controller/PlantDataRetrieval.java
+++ src/main/java/com/kueerickpatrick/plantinfo/controller/PlantDataRetrieval.java
@@ -2,12 +2,14 @@ package com.kueerickpatrick.plantinfo.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kueerickpatrick.plantinfo.entity.SingletonAttributes;
 import com.kueerickpatrick.plantinfo.entity.plantObjects.DataItem;
 import com.kueerickpatrick.plantinfo.entity.plantObjects.PlantDetailList;
+import com.kueerickpatrick.plantinfo.util.PropertiesLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,13 +18,19 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
 import java.util.List;
 
 @Path("/search")
 @Produces(MediaType.TEXT_HTML)
-public class PlantDataRetrieval extends HttpServlet {
+@WebServlet(
+        name = "PlantDataRetrieval",
+        value = "/PlantDataRetrieval"
+)
+public class PlantDataRetrieval implements PropertiesLoader {
     private final Logger logger = LogManager.getLogger(this.getClass());
     private static List<DataItem> foundPlants;
+    private SingletonAttributes attributes = SingletonAttributes.SingletonAttributes();
     @GET
     @Path("/{searchTerm}/get")
     public List<DataItem> getPlantFilteredInfo(@PathParam("searchTerm") String searchTerm) throws JsonProcessingException {
@@ -31,18 +39,17 @@ public class PlantDataRetrieval extends HttpServlet {
 
     public List<DataItem> getPlantDetails() throws JsonProcessingException {
         Client client = ClientBuilder.newClient();
-        for (int i = 1; i <= (int) getServletContext().getAttribute("numberOfPages"); i++) {
-            String plantInfoApiUrl = (String) getServletContext().getAttribute("plantListMainPageUrl") + i
-                    + "&" + getServletContext().getAttribute("apiKey");
+        foundPlants = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            String plantInfoApiUrl = attributes.plantListMainPage + i + "&"
+                    + attributes.apiKey;
             logger.info("Getting page number: " + i);
             logger.debug("Attempting to retrieve plant details pages");
             WebTarget targetPlantDetails = client.target(plantInfoApiUrl);
             String apiResponse = targetPlantDetails.request(MediaType.APPLICATION_JSON).get(String.class);
             ObjectMapper mapper = new ObjectMapper();
             PlantDetailList plantDetailList = mapper.readValue(apiResponse, PlantDetailList.class);
-            for (DataItem data: plantDetailList.getData()) {
-                foundPlants.add(data);
-            }
+            foundPlants.addAll(plantDetailList.getData());
             logger.debug("Current found plants in JSON data: " + foundPlants.size());
         }
         return foundPlants;

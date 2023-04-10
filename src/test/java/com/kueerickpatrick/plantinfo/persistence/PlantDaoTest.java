61e55e35d9f9b9219abbc0fabecd792b7ff2ddb5
Kue Xiong <kuexiong14@gmail.com>
2023-04-09 23:38:22 -0500

    test(PlantDaoTest): Added getByIdSuccess test

--- src/test/java/com/kueerickpatrick/plantinfo/persistence/PlantDaoTest.java
+++ src/test/java/com/kueerickpatrick/plantinfo/persistence/PlantDaoTest.java
@@ -1,19 +1,16 @@
 package com.kueerickpatrick.plantinfo.persistence;
 
 import com.kueerickpatrick.plantinfo.entity.Plant;
-import com.kueerickpatrick.plantinfo.entity.User;
-import com.kueerickpatrick.plantinfo.entity.Userplant;
 import com.kueerickpatrick.plantinfo.test.util.Database;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The type Plant dao test
@@ -46,24 +43,13 @@ public class PlantDaoTest {
         assertEquals(2,plants.size());
     }
 
-    /**
-     * Verifies add plant to user success
-     */
     @Test
-    void addPlantToUserSuccess() {
-        GenericDao userDao = new GenericDao(User.class);
-        User retrievedUser = (User)userDao.getById(2);
-
-        Plant firstNewPlant = new Plant(4);
-        Plant secondNewPlant = new Plant(5);
-
-        Set<Plant>
-
-        retrievedUser.setUserplants();
-
-        }
-
-        assertEquals(1,retrievedUser.getUserplants().size());
+    void getByIdSuccess(){
+        Plant retreivedPlant = (Plant)plantDao.getById(2);
+        assertNotNull(retreivedPlant);
+        int expected = 6;
+        int actual = retreivedPlant.getPerenualid();
+        assertEquals(expected, actual);
     }
 
 }

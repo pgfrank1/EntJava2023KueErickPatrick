07b2efc6baf77859b885cfffbd09373f8e258317
Kue Xiong <kuexiong14@gmail.com>
2023-04-10 02:50:07 -0500

    Merge from master branch

---
+++ src/main/java/com/kueerickpatrick/plantinfo/controller/PlantApplication.java
@@ -0,0 +1,26 @@
+/**
+ * PlantApplication
+ * Sets an application path for the REST service.
+ * @author ereyes3
+ */
+package com.kueerickpatrick.plantinfo.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/rest/")
+public class PlantApplication extends Application {
+    /**
+     * Gets classes needed for REST service.
+     * @return hashset of classes needed for REST.
+     */
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet restClasses = new HashSet<Class<?>>();
+        restClasses.add(PlantDataRetrieval.class);
+        restClasses.add(UserREST.class);
+        return restClasses;
+    }
+}

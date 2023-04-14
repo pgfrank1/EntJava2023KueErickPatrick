29fa0f7e0fd52d36ad7f16c4bcf8a34c8f0311cf
pgfrank1 <patrickfrank1995@gmail.com>
2023-04-14 17:34:02 -0500

    feat(controller): Now able to delete user plants from the database
    Author: pgfrank1
    Signed-off-by: pgfrank1 <patrickfrank1995@gmail.com>

--- src/main/java/com/kueerickpatrick/plantinfo/controller/DeletePlant.java
+++ src/main/java/com/kueerickpatrick/plantinfo/controller/DeletePlant.java
@@ -1,21 +1,19 @@
 package com.kueerickpatrick.plantinfo.controller;
 
-import com.kueerickpatrick.plantinfo.entity.Plant;
-import com.kueerickpatrick.plantinfo.entity.User;
 import com.kueerickpatrick.plantinfo.entity.Userplant;
 import com.kueerickpatrick.plantinfo.persistence.GenericDao;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.Session;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/**
+ * The type Delete plant.
+ */
 @WebServlet(
         name = "deletePlant",
         urlPatterns = {"/deletePlant"}
@@ -26,16 +24,12 @@ public class DeletePlant extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        HttpSession session = req.getSession();
 
-        GenericDao plantDao = new GenericDao(Plant.class);
-        GenericDao userDao = new GenericDao(User.class);
         GenericDao userplantDao = new GenericDao(Userplant.class);
 
         logger.debug("Delete string is: " + req.getParameter("plantId"));
 
-        Plant plantToRemove = new Plant(Integer.parseInt(req.getParameter("plantId")));
-        User currentUser = (User) userDao.getById((Integer) session.getAttribute("id"));
+        Userplant plantToRemove = (Userplant) userplantDao.getById(Integer.parseInt(req.getParameter("plantId")));
         userplantDao.delete(plantToRemove);
         String url = "/user";
         resp.sendRedirect(req.getContextPath() + url);
